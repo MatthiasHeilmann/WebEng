29f8f9cdf243122bd928382c558e69d1e1cf1590
Schwarz <schwas28@emea.roche.com>
2021-12-07 15:06:05 +0100

    MongoShit

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const http = require('http')
 const server = http.createServer(app);
 const port = 8080;
 const MongoDB = require('mongodb').MongoDB;
+const { MongoClient } = require('mongodb');
 
 // Starten des Webservers
 server.listen(port, function(){
@@ -17,13 +18,45 @@ app.use(express.static(__dirname));
 app.use(express.json());
 
 app.get('/' , (req, res) => {
-    res.sendFile('/scedule.html');
+    res.sendFile(__dirname + '/main.html');
 });
+
 /*
 app.get('/mongoTest', (rep, res) => {
     res.sendFile('/mongoTest.html');
 });
 */
+
 app.post('/mongoUpload', (req, res) => {
     // Upload req.body to mongoDB 
 });
+
+async function main(){
+
+    const uri = "mongodb+srv://webeng:ai21@cluster0.utsfr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+    const client = new MongoClient(uri);
+    try{
+        await client.connect();
+
+        await findLastPosts(client);
+
+    }catch (e){
+        console.error(e);
+    }finally{
+        await client.close();
+    }
+}
+
+main().catch(console.error);
+
+async function findLastPosts(client){
+    const cursor = await client.db("WebPosts").collection("posts").find();
+    const results = await cursor.toArray();
+    if(results){
+        console.log("Found results");
+        console.log(results);
+    }else{
+        console.log("nope");
+    }
+}
