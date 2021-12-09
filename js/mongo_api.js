a558b271627a71d0fe7396f92682373a1472bbbb
MatthiasHeilmann <78656019+MatthiasHeilmann@users.noreply.github.com>
2021-12-09 22:48:21 +0100

    POSTS FUNKTIONIEREN!!!

--- js/mongo_api.js
+++ js/mongo_api.js
@@ -1,6 +1,10 @@
 async function uploadToMongo(){
     var name = document.getElementById('name-input').value;
     var data = document.getElementById('blackboard').value;
+    
+    document.getElementById("blackboard").value='';
+    document.getElementById("name-input").value='';
+
     var jsonData = {name, data};
     console.log(`Sending: name:${name}, data:${data}`);
 
@@ -16,12 +20,28 @@ async function uploadToMongo(){
         },
         body: JSON.stringify(jsonData)
     }); 
-    
 }
 
+loadFromMongo();
+
 async function loadFromMongo(){
-    console.log("Fetching mogno: ");
+    console.log("Fetching mongo: ");
     var res = await fetch('/mongoGet');
-    console.log(await res.json());
+    var res2 = await res.json();
+    console.log(res2);
     console.log("Fetching done: ");
+    
+    for(var i=0;i<res2.data.length;i++){
+        let div = document.createElement('div');
+        div.classList.add('bubble');
+        div.classList.add('bubble-bottom-left');
+        let text = (res2.data[i].post || res2.data[i].data);
+        div.innerHTML=text+"</br>";
+        let cite = document.createElement('cite');
+        cite.innerHTML ="- "+ (res2.data[i].User || res2.data[i].name);
+        div.appendChild(cite);
+
+        document.getElementById("scroll-id").appendChild(div);
+    }
+    document.getElementById("loading-blackboard-loader").style.display="none";
 }
