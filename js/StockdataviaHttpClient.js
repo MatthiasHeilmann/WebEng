63570267f664ed510706cbeb82760d5e7887b528
LorenzMu <94124911+LorenzMu@users.noreply.github.com>
2021-12-10 19:29:42 +0100

    Zwei weitere Kurse hinzugefügt

--- js/StockdataviaHttpClient.js
+++ js/StockdataviaHttpClient.js
@@ -1,16 +1,18 @@
 //Function converts full JSON into list usable by chart.js
+var datapoints=[[],[],[]];
+            var alphabetize=[[],[],[]];
 
-function convertdata(datensatz){
+function convertdata(datensatz,index){
     let newdata=[];
     
     for(var x in datensatz["Weekly Adjusted Time Series"])
     {
-        alphabetize.push(x)
+        alphabetize[index].push(x)
     }
-    alphabetize.sort();
+    alphabetize[index].sort();
     //console.log(alphabetize);
-    for(let i=0;i<alphabetize.length;i++){
-        newdata.push(datensatz["Weekly Adjusted Time Series"][alphabetize[i]]["2. high"])
+    for(let i=0;i<alphabetize[index].length;i++){
+        newdata.push(datensatz["Weekly Adjusted Time Series"][alphabetize[index][i]]["5. adjusted close"])
     }
     //console.log(newdata);
     return newdata
@@ -33,41 +35,79 @@ var HttpClient = function() {
 //Instantiation of HttpClient & API Call RHO6.FRK=Roche, Max 5 calls per minute
 
 var client = new HttpClient();
-var StocksUrl = 'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol=RHO6.FRK&apikey=61EK505FXBJ59YGC';
-client.get(StocksUrl, function(response) {
+var StocksUrl1 = 'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol=RHO6.FRK&apikey=61EK505FXBJ59YGC';
+var StocksUrl2 = 'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol=DAI.FRK&apikey=61EK505FXBJ59YGC';
+var StocksUrl3 = 'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol=FRE.FRK&apikey=61EK505FXBJ59YGC';
+
+client.get(StocksUrl1, function(response) {
     var jsonfulldata = JSON.parse(response);
     //console.log(jsonfulldata)
-    datapoints= convertdata(jsonfulldata);
-    //console.log(datapoints)
+    datapoints[0]= convertdata(jsonfulldata,0);
+
+    client.get(StocksUrl2, function(response) {
+        var jsonfulldata = JSON.parse(response);
+        //console.log(jsonfulldata)
+        datapoints[1]= convertdata(jsonfulldata,1);
+
+        client.get(StocksUrl3, function(response) {
+            var jsonfulldata = JSON.parse(response);
+            //console.log(jsonfulldata)
+            datapoints[2]= convertdata(jsonfulldata,2);
+            //console.log(datapoints)
 
 
-    //console.log(datapoints)
-    //setTimeout(() => console.log(datapoints), 3000);
+            //console.log(datapoints)
+            //console.log(datapoints[0])
+            //console.log(datapoints[1])
+            //setTimeout(() => console.log(datapoints), 3000);
 
-    const ctx = document.getElementById('myChart').getContext('2d');
-    const myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: alphabetize,
-            datasets: [{
-                label: 'Stock Price of Roche Holding AG',
-                data: datapoints,
-                borderColor: 
-                    'rgba(235, 27, 42, 0.8)',
-                hoverBorderColor:
-                    'rgba(235, 27, 42, 0.3)',
-                tension: 0.1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true,
-                    max: datapoints.max
+            const ctx = document.getElementById('myChart').getContext('2d');
+            const myChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: alphabetize[0],
+                    datasets:
+                    [{
+                        label: 'Stock Price of Roche Holding AG',
+                        data: datapoints[0],
+                        borderColor: 
+                            'rgba(235, 27, 42, 0.8)',
+                        hoverBorderColor:
+                            'rgba(235, 27, 42, 0.3)',
+                        tension: 0.1
+                    },
+                    {
+                        label: 'Stock Price of Daimler AG',
+                        data: datapoints[1],
+                        borderColor: 
+                            'rgba(100, 100, 100, 0.8)',
+                        hoverBorderColor:
+                            'rgba(100, 100, 100, 0.3)',
+                        tension: 0.1
+                    },
+                    {
+                        label: 'Stock Price of Fresenius SE & Co. KGaA',
+                        data: datapoints[2],
+                        borderColor: 
+                            'rgba(200, 200, 200, 0.8)',
+                        hoverBorderColor:
+                            'rgba(200, 200, 200, 0.3)',
+                        tension: 0.1
+                    }
+                    
+                    ]
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true,
+                            max: datapoints[1].max
+                        }
+                        
+                    }
                 }
-                
-            }
-        }
-    });
-    //console.log(myChart.data.datasets.data)
-    });
+            });
+            //console.log(myChart.data.datasets.data)
+            });
+});
+});
